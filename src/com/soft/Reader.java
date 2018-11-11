7a43521b86924a5904681acf02b0ecd615f2bfc8
VitaliiKrasov <krasovvitalii@gmail.com>
2018-11-12 00:47:29 +0200

    complited Reader

--- src/com/soft/Reader.java
+++ src/com/soft/Reader.java
@@ -9,4 +9,14 @@ public class Reader {
         System.out.println("Enter number of HTTP Error:");
         return scanner.nextInt();
     }
+
+    static int readInt() {
+        System.out.println("Ebter integer number:");
+        return scanner.nextInt();
+    }
+
+    static float readFloat() {
+        System.out.println("Enter float number:");
+        return scanner.nextFloat();
+    }
 }
